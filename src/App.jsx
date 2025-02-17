68f80a7e09aa95b3e6247c2080fe9bb937d42504
mehmetklc35 <mehmetkilic35@outlook.com>
2025-02-17 16:25:12 +0300

    contact yapısı oluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./Components/About";
 import Collection from "./Components/Collection";
 import Hero from "./Components/Hero";
+import Contact from "./Components/Contact";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Hero />
       <About />
       <Collection />
+      <Contact /> 
     </>
   );
 }
